f4d1e20dddedc7bcaba9ffc7b8cae2f622bf8743
Jan Leissner <jleissner@uos.de>
2018-03-24 19:34:11 +0100

    added method from interface

--- src/main/java/ecs/systems/GravitySystem.java
+++ src/main/java/ecs/systems/GravitySystem.java
@@ -1,9 +1,16 @@
 package ecs.systems;
 
+import ecs.ID;
 import system.System;
+
+import java.util.List;
 
 /**
  * Created by Jan-Frederik Leißner on 24.03.2018.
  */
 public class GravitySystem extends System {
+    @Override
+    public void handle(List<ID> list) {
+
+    }
 }
