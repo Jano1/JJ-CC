da4bd2963e4c273017625ec710998484687f6821
Jan Leissner <jleissner@uos.de>
2018-03-24 18:27:52 +0100

    little test

---
+++ src/main/java/test_001.java
@@ -0,0 +1,14 @@
+package ecs;
+
+import input.ContextManager;
+
+/**
+ * Created by Jan-Frederik Leißner on 24.03.2018.
+ */
+public class test_001 {
+    public static void main(String[] args){
+        ContextManager m = new ContextManager();
+
+        System.out.println(m.contexts());
+    }
+}
