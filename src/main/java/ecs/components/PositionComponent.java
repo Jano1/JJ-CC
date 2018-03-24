d45ee039fa488947caec3aa61e248f86a26dc445
Jan Leissner <jleissner@uos.de>
2018-03-24 15:09:26 +0100

    renamed

--- src/main/java/ecs/components/PositionComponent.java
+++ src/main/java/ecs/components/PositionComponent.java
@@ -6,31 +6,31 @@ import org.joml.Vector3f;
 /**
  * Created by Jan-Frederik Leißner on 24.03.2018.
  */
-public class PRSComponent extends HistoricBasedComponent<PRSComponent> {
+public class PositionComponent extends HistoricBasedComponent<PositionComponent> {
 
     Vector3f position;
     Vector3f rotation;
     Vector3f scaling;
 
-    public PRSComponent(Vector3f position, Vector3f rotation, Vector3f scaling) {
+    public PositionComponent(Vector3f position, Vector3f rotation, Vector3f scaling) {
         this.position = position;
         this.rotation = rotation;
         this.scaling = scaling;
     }
 
-    public PRSComponent(Vector3f position){
+    public PositionComponent(Vector3f position){
         this(position,new Vector3f(0,0,0),new Vector3f(0,0,0));
     }
 
-    public PRSComponent(){
+    public PositionComponent(){
         this(new Vector3f(0,0,0));
     }
 
     @Override
-    public PRSComponent absolute() {
+    public PositionComponent absolute() {
         if(has_base()){
-            PRSComponent base = base().absolute();
-            return new PRSComponent(
+            PositionComponent base = base().absolute();
+            return new PositionComponent(
                     position.add(base.position),
                     rotation.add(base.rotation),
                     scaling.add(base.scaling)
@@ -50,13 +50,13 @@ public class PRSComponent extends HistoricBasedComponent<PRSComponent> {
     }
 
     @Override
-    public boolean equal_values(PRSComponent PRSComponent) {
-        return position.equals(PRSComponent.position) && rotation.equals(PRSComponent.rotation) && scaling.equals(PRSComponent.scaling);
+    public boolean equal_values(PositionComponent PositionComponent) {
+        return position.equals(PositionComponent.position) && rotation.equals(PositionComponent.rotation) && scaling.equals(PositionComponent.scaling);
     }
 
     @Override
-    public PRSComponent clone() {
-        return new PRSComponent(
+    public PositionComponent clone() {
+        return new PositionComponent(
                 new Vector3f(position.x,position.y,position.z),
                 new Vector3f(rotation.x,rotation.y,rotation.z),
                 new Vector3f(scaling.x,scaling.y,scaling.z)
