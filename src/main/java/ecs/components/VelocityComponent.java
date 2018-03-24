e3d343d24ed9451b8980621df79b698099f22d9b
Jan Leissner <jleissner@uos.de>
2018-03-24 15:12:35 +0100

    added velocity component

---
+++ src/main/java/ecs/components/VelocityComponent.java
@@ -0,0 +1,55 @@
+package ecs.components;
+
+import component.BasedComponent;
+import org.joml.Vector3f;
+
+/**
+ * Created by Jan-Frederik Leißner on 24.03.2018.
+ */
+public class VelocityComponent extends BasedComponent<VelocityComponent> {
+
+    Vector3f position; // per second
+    Vector3f rotation; // per second
+    Vector3f scaling;  // per second
+
+    public VelocityComponent(Vector3f position, Vector3f rotation, Vector3f scaling) {
+        this.position = position;
+        this.rotation = rotation;
+        this.scaling = scaling;
+    }
+
+    public VelocityComponent(Vector3f position){
+        this(position,new Vector3f(0,0,0),new Vector3f(0,0,0));
+    }
+
+    public VelocityComponent(){
+        this(new Vector3f(0,0,0));
+    }
+
+    @Override
+    public VelocityComponent absolute() {
+        if(has_base()){
+            VelocityComponent base = base().absolute();
+            return new VelocityComponent(
+                    position.add(base.position),
+                    rotation.add(base.rotation),
+                    scaling.add(base.scaling)
+            );
+        }
+        return this;
+    }
+
+    @Override
+    public boolean equal_values(VelocityComponent velocityComponent) {
+        return position.equals(velocityComponent.position) && rotation.equals(velocityComponent.rotation) && scaling.equals(velocityComponent.scaling);
+    }
+
+    @Override
+    public VelocityComponent clone() {
+        return new VelocityComponent(
+                new Vector3f(position.x,position.y,position.z),
+                new Vector3f(rotation.x,rotation.y,rotation.z),
+                new Vector3f(scaling.x,scaling.y,scaling.z)
+        );
+    }
+}
