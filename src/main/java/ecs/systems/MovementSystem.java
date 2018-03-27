450143ce42e317053a9269fcf1d73d07dee3a5a4
Jan Leissner <jleissner@uos.de>
2018-03-27 19:57:49 +0200

    movement works

--- src/main/java/ecs/systems/MovementSystem.java
+++ src/main/java/ecs/systems/MovementSystem.java
@@ -19,6 +19,9 @@ public class MovementSystem extends System {
         super(PositionComponent.class,VelocityComponent.class,AccelerationComponent.class);
     }
 
+    private int counter = 0;
+    private int print_at = 50;
+
     public void handle(List<ID> to_handle) {
         for (ID single_id : to_handle) {
             PositionComponent position = single_id.get(PositionComponent.class);
@@ -29,11 +32,20 @@ public class MovementSystem extends System {
             float delta_t = single_id.get(TimeComponent.class).delta_t();
             float delta_t2 = delta_t * delta_t;
 
+            if(counter >= 10){
+                java.lang.System.out.println("pos add v("+delta_t+"): "+velocity.position.mul(delta_t, new Vector3f(0,0,0)));
+                java.lang.System.out.println("pos add a("+delta_t2+"): "+velocity.position.mul(delta_t, new Vector3f(0,0,0)));
+            }
+
             //handle position
             position.position.add(velocity.position.mul(delta_t, new Vector3f(0,0,0)));
             position.position.add(acceleration.position.mul(delta_t2, new Vector3f(0,0,0)));
 
-            java.lang.System.out.println("Moved to: "+position.position);
+            if(counter >= 10){
+                java.lang.System.out.println("Moved to: "+position.position);
+                counter = 0;
+            }
+            counter++;
 
             //handle rotation
             position.rotation.add(velocity.rotation.mul(delta_t, new Vector3f(0,0,0)));
