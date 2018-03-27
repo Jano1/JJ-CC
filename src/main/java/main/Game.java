812c4623f063e00000f348604a75ea479f7e2657
Jan Leissner <jleissner@uos.de>
2018-03-27 15:07:27 +0200

    ready for merge with master

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,25 +1,44 @@
 package main;
 
 import ecs.ECS;
+import org.apache.commons.collections4.map.HashedMap;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Jan-Frederik Leißner on 27.03.2018.
  */
 public class Game implements Runnable{
 
-    public static final int TARGET_FPS = 75;
+    public static void main(String[] args){
+        Game game = new Game();
+        Thread thread = new Thread(game);
+        thread.start();
+    }
 
-    public static final int TARGET_UPS = 30;
+    Map<String,ECS> ecs_list;
+    List<String> to_tick;
 
-    private ECS ecs;
+    public Game(){
+        ecs_list = new HashedMap<>();
+        to_tick = new ArrayList<>();
+    }
 
     @Override
     public void run() {
         try {
+            init();
             loop();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void init() {
+        ecs_list.put("play_level",new ECS(10000));
+        to_tick.add("play_level");
     }
 
     protected void loop() {
@@ -29,35 +48,63 @@ public class Game implements Runnable{
 
         long next_game_tick = System.nanoTime();
 
-        int loops = 0;
-        float interpolation = 1f;
+        int loops;
+        float interpolation;
+
+        XPSCounter ups = new XPSCounter("UPS");
+        XPSCounter fps = new XPSCounter("FPS");
 
         boolean running = true;
         while (running) {
-            input();
             loops = 0;
             while( System.nanoTime() > next_game_tick && loops < max_frameskip) {
                 update();
                 next_game_tick += skip_ticks;
                 loops++;
+                ups.tick();
             }
             interpolation = (System.nanoTime() + skip_ticks - next_game_tick) / skip_ticks;
             render(interpolation);
+            fps.tick();
         }
-
-
-
-    }
-
-    private void input() {
-        
-    }
-
-    private void update() {
 
     }
 
     private void render(float interpolation) {
+        for(String name : to_tick){
+            ecs_list.get(name).render(interpolation);
+        }
+    }
 
+    private void update() {
+        for(String name : to_tick){
+            ecs_list.get(name).tick();
+        }
+    }
+}
+
+class XPSCounter{
+    long start;
+    int xps;
+    String name;
+
+    public XPSCounter(String name,long start){
+        this.name = name;
+        this.start = start;
+        this.xps = 0;
+    }
+
+    public XPSCounter(String name){
+        this(name,System.nanoTime());
+    }
+
+    public void tick(){
+        if(System.nanoTime() <=  start+(1000000000)){
+            xps++;
+        }else{
+            System.out.println(name+": "+xps);
+            xps = 0;
+            start = System.nanoTime();
+        }
     }
 }
