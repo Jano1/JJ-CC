3d9ce4a456a6f9bc2e8ea17511da426a224dd3c8
Jan Leissner <jleissner@uos.de>
2018-03-27 14:12:22 +0200

    commmit

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -1,10 +1,23 @@
 package main;
 
+import ecs.ECS;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Jan-Frederik Leißner on 27.03.2018.
  */
 public class Game {
+
+    Map<String, ECS> ecs_list;
+    List<String> to_tick;
+
     public static void main(String[] args){
+
+    }
+
+    public void loop() {
 
     }
 }
