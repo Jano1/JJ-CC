66733be158f44ccd335e504317b60a3ffea22556
Jan Leissner <jleissner@uos.de>
2018-03-28 17:05:54 +0200

    added material component

---
+++ src/main/java/ecs/components/MaterialComponent.java
@@ -0,0 +1,34 @@
+package ecs.components;
+
+import component.Component;
+import org.joml.Vector4f;
+
+/**
+ * Created by Jan-Frederik Leißner on 28.03.2018.
+ */
+public class MaterialComponent extends Component<MaterialComponent>{
+
+    public static final Vector4f DEFAULT = new Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+    private Vector4f ambient_color;
+    private Vector4f diffuse_color;
+    private Vector4f specular_color;
+    private float reflectance;
+
+    public MaterialComponent(Vector4f ambient_color, Vector4f diffuse_color, Vector4f specular_color, float reflectance) {
+        this.ambient_color = ambient_color;
+        this.diffuse_color = diffuse_color;
+        this.specular_color = specular_color;
+        this.reflectance = reflectance;
+    }
+
+    @Override
+    public boolean equal_values(MaterialComponent materialComponent) {
+        return false;
+    }
+
+    @Override
+    public MaterialComponent clone() {
+        return new MaterialComponent(ambient_color,diffuse_color,specular_color,reflectance);
+    }
+}
--- src/main/java/ecs/components/TextureComponent.java
+++ src/main/java/ecs/components/TextureComponent.java
@@ -7,19 +7,21 @@ import component.Component;
  */
 public class TextureComponent extends Component<TextureComponent> {
 
-    private String texture_image_name;
+    private String image_name;
+    private String heightmap_image_name;
 
-    public TextureComponent(String texture_image_name) {
-        this.texture_image_name = texture_image_name;
+    public TextureComponent(String image_name, String heightmap_image_name) {
+        this.image_name = image_name;
+        this.heightmap_image_name = heightmap_image_name;
     }
 
     @Override
     public boolean equal_values(TextureComponent textureComponent) {
-        return textureComponent.texture_image_name.equals(texture_image_name);
+        return textureComponent.image_name.equals(image_name) && textureComponent.heightmap_image_name.equals(heightmap_image_name);
     }
 
     @Override
     public TextureComponent clone() {
-        return new TextureComponent(texture_image_name);
+        return new TextureComponent(image_name,heightmap_image_name);
     }
 }
